ba1f6a657513456b6a12d1cf738dfa3be76830d0
Josue Vicente <josue.vicente@ufl.edu>
2022-03-29 04:29:43 -0400

    Squashed commit of the following:
    commit 0a5c11d5c5be7ed9e4f05d117a44512c0f213fdd
    Merge: 6f6c2bd b7a8029
    Author: brod2240 <87546899+brod2240@users.noreply.github.com>
    Date:   Mon Mar 28 17:26:12 2022 -0400
    
        Merge branch 'Brianna' into test
    
    commit b7a8029a155ecccdeeb374345a30c817622a6d1d
    Author: brod2240 <brianna.rodriguez2240@gmail.com>
    Date:   Mon Mar 28 17:19:44 2022 -0400
    
        Confirm Page
    
    commit 6f6c2bdd240409d9b13313817d8853d70ce576d0
    Merge: 085dc6b a5b99bc
    Author: Matthew Flippen <40397981+mflippen@users.noreply.github.com>
    Date:   Sun Mar 27 14:57:13 2022 -0400
    
        Merge branch 'jonathan' into test
    
    commit a5b99bc16f30e2c590e62a8428265eec95528936
    Author: Jonathan Kahn <63828254+kjonathan024@users.noreply.github.com>
    Date:   Sun Mar 27 11:05:09 2022 -0400
    
        Added MongoDB Realm to gradle
    
        Added MongoDB Realm Plugin to Gradle Files (both app and project level files)
    
    commit 085dc6b234fe85a0a57161a1c58127f0b78e4934
    Author: Matthew <flpn4life@gmail.com>
    Date:   Mon Mar 21 15:03:51 2022 -0400
    
        Added blank login and confirmation pages

---
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.ace_club_application.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+}
--- ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
@@ -1,14 +1,23 @@
 package com.ace_club_application.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.aceapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void confirmCode(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+    }
+
 }
