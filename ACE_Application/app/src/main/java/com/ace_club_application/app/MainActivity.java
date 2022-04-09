3d49927d2ec74cf81722082b2a9cec9ada970612
Josue Vicente <josue.vicente@ufl.edu>
2022-04-09 02:57:52 -0400

    Test
    Adds Task and Task Status

--- ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
@@ -3,6 +3,8 @@ package com.ace_club_application.app;
 import com.ace_club_application.app.BuildConfig;
 
 import io.realm.Realm;
+import io.realm.RealmCollection;
+import io.realm.RealmConfiguration;
 import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
 
@@ -10,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
     //TODO: replace with buildConfig in gradle file
     String Appid = "ace_android_application-sqneg";
+
+    Realm testRealm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
         //Initializes MongoDB realm on application
         Realm.init(this);
 
-        App app = new App(new AppConfiguration.Builder(Appid).build());
+        RealmConfiguration config = new RealmConfiguration.Builder().name(Appid).build();
+
+        testRealm = Realm.getInstance(config);
+
     }
 
     public void confirmCode(View view) {
