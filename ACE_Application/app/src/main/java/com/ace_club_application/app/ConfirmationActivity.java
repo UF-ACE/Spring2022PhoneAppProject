31ec2af0b44319715bf3b6aab0f41e8418608c18
Josue Vicente <josue.vicente@ufl.edu>
2022-04-10 06:38:38 -0400

    Testing
    Connecting

--- ACE_Application/app/src/main/java/com/ace_club_application/app/ConfirmationActivity.java
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/ConfirmationActivity.java
@@ -3,4 +3,5 @@ package com.ace_club_application.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ConfirmationActivity extends AppCompatActivity {
+
 }
--- ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
@@ -1,47 +1,82 @@
 package com.ace_club_application.app;
 
-import com.ace_club_application.app.BuildConfig;
-
-import io.realm.Realm;
-import io.realm.RealmCollection;
-import io.realm.RealmConfiguration;
-import io.realm.mongodb.App;
-import io.realm.mongodb.AppConfiguration;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
-
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.bson.Document;
+
+import io.realm.Realm;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
+import io.realm.mongodb.mongo.MongoClient;
+import io.realm.mongodb.mongo.MongoCollection;
+import io.realm.mongodb.mongo.MongoDatabase;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.aceapp.MESSAGE";
+    String appId = "testapp-mftvr";
 
-    //TODO: replace with buildConfig in gradle file
-    String Appid = "ace_android_application-sqneg";
-
-    Realm testRealm;
-
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openConfirmation();
+            }
+        });
+
+
+
         //Initializes MongoDB realm on application
         Realm.init(this);
+        App app = new App(new AppConfiguration.Builder(appId).build());
 
-        RealmConfiguration config = new RealmConfiguration.Builder().name(Appid).build();
 
-        testRealm = Realm.getInstance(config);
-
+        //Logs a user in to the realm, tells users if successful or not
+        Credentials credentials = Credentials.emailPassword("email", "pass");
+        app.loginAsync(credentials, new App.Callback<User>() {
+            @Override
+            public void onResult(App.Result<User> result) {
+                if (result.isSuccess()) {
+                    Log.v("User", "Logged in anonymously");
+                }
+                else {
+                    Log.v("User", "failed to log in");
+                }
+            }
+        });
     }
 
-    public void confirmCode(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    public void openConfirmation() {
+        Intent intent = new Intent(this, ConfirmationActivity.class);
         startActivity(intent);
     }
-
 }
