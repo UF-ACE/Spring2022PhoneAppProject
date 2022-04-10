f1fbc3c4fd19531d5698ef463dfbed433e44ec10
Josue Vicente <josue.vicente@ufl.edu>
2022-04-10 08:57:26 -0400

    More Testing
    testing user addition
    for app usage, needs to change app ID to ACE App

--- ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
@@ -1,79 +1,86 @@
 package com.ace_club_application.app;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.Scopes;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-
-import org.bson.Document;
 
 import io.realm.Realm;
 import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
 import io.realm.mongodb.Credentials;
 import io.realm.mongodb.User;
-import io.realm.mongodb.mongo.MongoClient;
-import io.realm.mongodb.mongo.MongoCollection;
-import io.realm.mongodb.mongo.MongoDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.aceapp.MESSAGE";
     String appId = "testapp-mftvr";
 
     private Button button;
+    private EditText email;
+    private EditText password;
+    private TextView textView;
+
+    String emailString = "email";
+    String passwordString = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Displays main page and initializes MongoDB realm on application
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
+        Realm.init(this);
+        App app = new App(new AppConfiguration.Builder(appId).build());
+
+        //Get EditText boxes from main and button
+        email = (EditText) findViewById(R.id.editMainEmailAddress);
+        password = (EditText) findViewById(R.id.editMainPassword);
+
+        button = (Button) findViewById(R.id.mainLoginButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                emailString = email.getText().toString();
+                passwordString = password.getText().toString();
+                Credentials credentials = Credentials.emailPassword(emailString, passwordString);
+                
+                //Logs a user in to the realm, tells users if successful or not
+                app.loginAsync(credentials, new App.Callback<User>() {
+                    @Override
+                    public void onResult(App.Result<User> result) {
+                        if (result.isSuccess()) {
+                            Log.v("User", "Logged in anonymously");
+                        } else {
+                            Log.v("User", "failed to log in");
+                        }
+                    }
+                });
                 openConfirmation();
             }
         });
 
 
 
-        //Initializes MongoDB realm on application
-        Realm.init(this);
-        App app = new App(new AppConfiguration.Builder(appId).build());
 
 
-        //Logs a user in to the realm, tells users if successful or not
-        Credentials credentials = Credentials.emailPassword("email", "pass");
-        app.loginAsync(credentials, new App.Callback<User>() {
-            @Override
-            public void onResult(App.Result<User> result) {
-                if (result.isSuccess()) {
-                    Log.v("User", "Logged in anonymously");
-                }
-                else {
-                    Log.v("User", "failed to log in");
-                }
-            }
-        });
+//        app.getEmailPassword().registerUserAsync(emailString, passwordString, it->{
+//            if (it.isSuccess()) {
+//                Log.v("User", "new user registered");
+//            }
+//            else {
+//                Log.v("User", "failed to log in");
+//            }
+//
+//        });
+
     }
+
 
     public void openConfirmation() {
         Intent intent = new Intent(this, ConfirmationActivity.class);
