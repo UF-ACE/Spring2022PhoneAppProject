5a7f2e8aeb4bdd1a7f7dea1ebc88827e0cf0c3b7
Matthew <flpn4life@gmail.com>
2022-04-17 16:01:41 -0400

    Connected Login Page to MongoDB Realm

--- ACE_Application/app/src/main/java/com/ace_club_application/app/LoginActivity.java
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/LoginActivity.java
@@ -9,10 +9,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button confirmLoginButton;
+    EditText emailInput;
+    EditText passwordInput;
+
+    String username;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +26,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         confirmLoginButton = (Button) findViewById(R.id.confirmLoginButton);
+        emailInput = (EditText) findViewById(R.id.emailAddressText);
+        passwordInput = (EditText) findViewById(R.id.passwordText);
+
         confirmLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                username = emailInput.getText().toString();
+                password = passwordInput.getText().toString();
                 openConfirmation(v);
             }
         });
@@ -30,7 +41,10 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void openConfirmation(View v) {
-        Intent intent = new Intent(this, ConfirmationActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("username", username);
+        intent.putExtra("password", password);
+        intent.putExtra("login", true);
         startActivity(intent);
     }
 }
--- ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/MainActivity.java
@@ -3,10 +3,13 @@ package com.ace_club_application.app;
 import io.realm.Realm;
 import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -23,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        String username = null;
+        String password = null;
+        Boolean login = false;
+
+        if (extras != null)
+        {
+            username = getIntent().getStringExtra("username");
+            password = getIntent().getStringExtra("password");
+            login = getIntent().getBooleanExtra("login", true);
+        }
+
+
+
         loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +54,43 @@ public class MainActivity extends AppCompatActivity {
         Realm.init(this);
 
         App app = new App(new AppConfiguration.Builder(Appid).build());
+
+        //If we have a valid username and password
+        if (username != null && password != null)
+        {
+            //If we're coming from the login page, login to the app
+            if (login)
+            {
+                Credentials credentials = Credentials.emailPassword(username, password);
+                app.loginAsync(credentials, new App.Callback<User>() {
+                    @Override
+                    public void onResult(App.Result<User> result) {
+                        if (result.isSuccess())
+                        {
+                            Log.v("User", "logged in via email and password");
+                        }
+                        else
+                        {
+                            Log.v("User", "failed to login");
+                        }
+                    }
+                });
+            }
+            //Otherwise, create a new user with the username and password.
+            else
+            {
+                app.getEmailPassword().registerUserAsync(username, password, it->{
+                    if (it.isSuccess()){
+                        Log.v("User", "registered with email");
+                    }
+                    else{
+                        Log.v("User", "was unsuccessful in registration");
+                    }
+                });
+            }
+        }
+
+
     }
 
     public void openLogin(View v) {
