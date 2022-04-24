f804dc6ca75e122e6f6a115bbb0a5ece744321cb
Matthew <flpn4life@gmail.com>
2022-04-23 20:30:01 -0400

    Changes to Registration Page
    Users now upload registration data upon registering and there is now some sort of confirm password validation to ensure the 2 passwords are the same.

--- ACE_Application/app/src/main/java/com/ace_club_application/app/LoginActivity.java
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/LoginActivity.java
@@ -8,10 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.bson.Document;
+
 import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
 import io.realm.mongodb.Credentials;
 import io.realm.mongodb.User;
+import io.realm.mongodb.mongo.MongoClient;
+import io.realm.mongodb.mongo.MongoCollection;
+import io.realm.mongodb.mongo.MongoDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -46,10 +51,38 @@ public class LoginActivity extends AppCompatActivity {
                 Credentials credentials = Credentials.emailPassword(email, password);
                 app.loginAsync(credentials, new App.Callback<User>() {
                     @Override
-                    public void onResult(App.Result<User> result) {
-                        if (result.isSuccess())
+                    public void onResult(App.Result<User> it) {
+                        if (it.isSuccess())
                         {
                             Log.v("User", "logged in via email and password");
+//                            User user = app.currentUser();
+//                            MongoClient mongoClient =
+//                                    user.getMongoClient("mongodb-atlas"); // service for MongoDB Atlas cluster containing custom user data
+//                            MongoDatabase mongoDatabase =
+//                                    mongoClient.getDatabase("ACEsite");
+//                            MongoCollection<Document> mongoCollection =
+//                                    mongoDatabase.getCollection("users");
+//                            String name = "name";
+//                            String major = "major";
+//                            int year = 1;
+//                            String phoneNumber = "123-456-7890";
+//                            mongoCollection.insertOne(
+//                                    new Document("userid", user.getId())
+//                                            .append("name", name)
+//                                            .append("email", email)
+//                                            .append("phoneNumber", phoneNumber)
+//                                            .append("major", major)
+//                                            .append("year", year)
+//                                            .append("score", 0)
+//                                            .append("tier", "Standard"))
+//                                    .getAsync(result -> {
+//                                        if (result.isSuccess()) {
+//                                            Log.v("EXAMPLE", "Inserted custom user data document. _id of inserted document: "
+//                                                    + result.get().getInsertedId());
+//                                        } else {
+//                                            Log.e("EXAMPLE", "Unable to insert custom user data. Error: " + result.getError());
+//                                        }
+//                                    });
                         }
                         else
                         {
--- ACE_Application/app/src/main/java/com/ace_club_application/app/RegistrationActivity.java
+++ ACE_Application/app/src/main/java/com/ace_club_application/app/RegistrationActivity.java
@@ -3,15 +3,23 @@ package com.ace_club_application.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.bson.Document;
 
 import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.User;
+import io.realm.mongodb.mongo.MongoClient;
+import io.realm.mongodb.mongo.MongoCollection;
+import io.realm.mongodb.mongo.MongoDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -59,22 +67,59 @@ public class RegistrationActivity extends AppCompatActivity {
                 email = emailInput.getText().toString();
                 password = passwordInput.getText().toString();
                 confirmPassword = confirmPasswordInput.getText().toString();
-                app.getEmailPassword().registerUserAsync(email, password, it->{
-                    if (it.isSuccess()){
-                        Log.v("User", "registered with email");
-                    }
-                    else{
-                        Log.v("User", "was unsuccessful in registration");
-                    }
-                });
-                confirmRegistration(v);
+
+                if (!password.equals(confirmPassword))
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Passwords do not match";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                else
+                {
+                    app.getEmailPassword().registerUserAsync(email, password, it->{
+                        if (it.isSuccess()){
+                            Log.v("User", "registered with email");
+                            User user = app.currentUser();
+                            MongoClient mongoClient =
+                                    user.getMongoClient("mongodb-atlas"); // service for MongoDB Atlas cluster containing custom user data
+                            MongoDatabase mongoDatabase =
+                                    mongoClient.getDatabase("ACEsite");
+                            MongoCollection<Document> mongoCollection =
+                                    mongoDatabase.getCollection("users");
+                            mongoCollection.insertOne(
+                                    new Document("userid", user.getId())
+                                            .append("name", name)
+                                            .append("email", email)
+                                            .append("phoneNumber", phoneNumber)
+                                            .append("major", major)
+                                            .append("year", year)
+                                            .append("score", 0)
+                                            .append("tier", "Standard"))
+                                    .getAsync(result -> {
+                                        if (result.isSuccess()) {
+                                            Log.v("EXAMPLE", "Inserted custom user data document. _id of inserted document: "
+                                                    + result.get().getInsertedId());
+                                        } else {
+                                            Log.e("EXAMPLE", "Unable to insert custom user data. Error: " + result.getError());
+                                        }
+                                    });
+                        }
+                        else{
+                            Log.v("User", "was unsuccessful in registration");
+                        }
+                    });
+                    confirmRegistration(v);
+                }
             }
         });
 
     }
 
     public void confirmRegistration(View v) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         intent.putExtra("name", name);
         intent.putExtra("major", major);
         intent.putExtra("year", year);
